5c8d64a4b985955665ace4dd6749e37327b34925
Jesse Lentz <jesselentz@gmail.com>
2021-03-03 12:46:39 -0500

    Update testing

--- testing.mjs
+++ testing.mjs
@@ -1,5 +1,5 @@
-import puppeteer from 'puppeteer';
 import assert from 'assert';
+import puppeteer from 'puppeteer';
 
 const browser = await puppeteer.launch();
 
@@ -9,12 +9,14 @@ await page.goto('http://www.iotait.com');
 
 // Wait for element to be visible with a timeout
 // This will throw an error if the timeout is exceeded
-await page.waitForSelector('.banner-social-buttons a', { timeout: 5000 });
+await page.waitForSelector('.banner-social-buttons a', { timeout: 3000 });
 
 // Get elements by selector without waiting
 const buttons = await page.$('.banner-social-buttons a');
 
 // Assert that they are not falsey
 assert.ok(buttons);
+
+console.log('Test PASSED!');
 
 await browser.close();
